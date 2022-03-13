7bb8b30b75d13b5cd7261c4767debbe657fb804d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-03-13 01:33:20 +0100

    feat: add usage instructions and imrpove layout

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,28 +1,40 @@
+import Image from "next/image";
 import { nonogramsSelector, useStore } from "../lib/store";
 
 export function Gallery() {
   const nonograms = useStore(nonogramsSelector);
+  const hasNonograms = nonograms.length > 0;
 
   return (
-    <div className="rounded-xl border-4 border-dashed border-sky-500 p-6 text-center">
-      Gallery
-      <ul>
-        {nonograms.map((nonogram) => (
-          <li key={nonogram.filename}>
-            {JSON.stringify(nonogram, null, 2)}
-            {nonogram.image && (
-              //   <Image src={nonogram.image} alt="" width={500} height={500} />
-              <img
-                src={nonogram.image}
-                alt=""
-                width={500}
-                height={500}
-                className="pixel-art"
-              />
-            )}
-          </li>
-        ))}
-      </ul>
+    <div>
+      {hasNonograms ? (
+        <p>
+          Found {nonograms.length} nonogram{nonograms.length > 1 && "s"}.
+        </p>
+      ) : (
+        <p>No nonograms found, try selecting some files.</p>
+      )}
+
+      {hasNonograms && (
+        <div className="mt-8 flex flex-wrap justify-center gap-8">
+          {nonograms.map(({ filename, image }) => (
+            <div key={filename}>
+              {/* {JSON.stringify(nonogram, null, 2)} */}
+              {image && (
+                <div className="rounded border border-gray-500 p-4">
+                  <Image
+                    src={image}
+                    alt=""
+                    width={150}
+                    height={150}
+                    className="pixel-art object-contain"
+                  />
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
