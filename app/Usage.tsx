eb1c68b684f9a1f45a78671d7eab2eb61ab6bbaf
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-03-28 14:36:54 +0200

    refactor: extract Usage

---
+++ app/Usage.tsx
@@ -0,0 +1,50 @@
+export default function Usage() {
+  return (
+    <>
+      <h2>Usage</h2>
+      <p>
+        With this tool, nonogram authors can easily convert their own designs
+        from the <code>.ujc</code> files exported from the app to normal{" "}
+        <code>.png</code> images as follows.
+      </p>
+
+      <ul>
+        <li>
+          <p>
+            <strong>Step 1: Export Your Data From The App</strong>
+          </p>
+          <p>
+            To start, open the Nonograms Katana Android app, then tap on the{" "}
+            <code>Settings</code> icon in the top right corner; once in the
+            settings, open the sub-menu named <code>Other</code> and select{" "}
+            <code>Save progress to file (zip)</code> to export an archive of
+            your data.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Step 2: Extract The Data Archive</strong>
+          </p>
+          <p>
+            Using your preferred file manager, extract the{" "}
+            <code>NonogramsKatana.zip</code> archive so that the sub-folder
+            named <code>MyNonograms</code> becomes accessible. This folder
+            contains all your nonograms saved as <code>.ujc</code> files.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Step 3: Drag and Drop Your Files</strong>
+          </p>
+          <p>
+            Now, you can drag and drop the <code>.ujc</code> files in the area
+            below; alternatively, you can click or tap on the area to open a
+            file picker dialog where you can select the files. After a short
+            while, the converted images will appear in the gallery below ready
+            to download.
+          </p>
+        </li>
+      </ul>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { compress } from "compress-tag";
 import type { Metadata } from "next";
 import Header from "./Header";
 import Link from "./Link";
+import Usage from "./Usage";
 
 const title = "Download your Nonograms Katana puzzles as images";
 const description = compress`
@@ -28,50 +29,7 @@ export default function IndexPage() {
       <Header />
       <hr />
 
-      <h2>Usage</h2>
-      <p>
-        With this tool, nonogram authors can easily convert their own designs
-        from the <code>.ujc</code> files exported from the app to normal{" "}
-        <code>.png</code> images as follows.
-      </p>
-
-      <ul>
-        <li>
-          <p>
-            <strong>Step 1: Export Your Data From The App</strong>
-          </p>
-          <p>
-            To start, open the Nonograms Katana Android app, then tap on the{" "}
-            <code>Settings</code> icon in the top right corner; once in the
-            settings, open the sub-menu named <code>Other</code> and select{" "}
-            <code>Save progress to file (zip)</code> to export an archive of
-            your data.
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Step 2: Extract The Data Archive</strong>
-          </p>
-          <p>
-            Using your preferred file manager, extract the{" "}
-            <code>NonogramsKatana.zip</code> archive so that the sub-folder
-            named <code>MyNonograms</code> becomes accessible. This folder
-            contains all your nonograms saved as <code>.ujc</code> files.
-          </p>
-        </li>
-        <li>
-          <p>
-            <strong>Step 3: Drag and Drop Your Files</strong>
-          </p>
-          <p>
-            Now, you can drag and drop the <code>.ujc</code> files in the area
-            below; alternatively, you can click or tap on the area to open a
-            file picker dialog where you can select the files. After a short
-            while, the converted images will appear in the gallery below ready
-            to download.
-          </p>
-        </li>
-      </ul>
+      <Usage />
 
       <h2>TODO: Dropzone</h2>
 
