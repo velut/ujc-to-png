a52d3805a2b6be3b67d073062eb0b99bfed807f7
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-03-30 21:00:05 +0200

    feat: update ujc files atom

--- app/store.ts
+++ app/store.ts
@@ -1,5 +1,18 @@
 import { atom } from "jotai";
 
-export const ujcFilesAtom = atom<File[]>([]);
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const _ujcFilesAtom = atom<File[]>([]);
+
 export const nongramsAtom = atom([]);
 export const loadingAtom = atom(false);
+
+export const ujcFilesAtom = atom(
+  (get) => get(_ujcFilesAtom),
+  async (get, set, files: File[]) => {
+    set(loadingAtom, true);
+    await sleep(5000);
+    set(_ujcFilesAtom, files);
+    set(loadingAtom, false);
+  }
+);
