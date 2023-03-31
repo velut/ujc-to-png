a63911cc2f43c65bb6786222984496926e6507f5
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-01 01:29:16 +0200

    feat: update store to use internal state atoms

--- app/store.ts
+++ app/store.ts
@@ -4,15 +4,25 @@ import { parseNonograms } from "../lib/parse-nonogram";
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
-export const loadingAtom = atom(false);
-export const nonogramsAtom = atom<Nonogram[]>([]);
+const _loadingAtom = atom(false);
+const _nonogramsAtom = atom<Nonogram[]>([]);
+const _renderOptionsAtom = atom({ recolor: false, scale: 1 });
+const _imageFilesAtom = atom<File[]>([]);
 
+export const loadingAtom = atom((get) => get(_loadingAtom));
+export const nonogramsAtom = atom((get) => get(_nonogramsAtom));
 export const hasNonogramsAtom = atom((get) => get(nonogramsAtom).length > 0);
+export const imageFilesAtom = atom((get) => get(_imageFilesAtom));
 
 export const processFilesAtom = atom(null, async (get, set, files: File[]) => {
-  set(loadingAtom, true);
-  set(nonogramsAtom, []);
+  set(_loadingAtom, true);
+  set(_nonogramsAtom, []);
+  set(_imageFilesAtom, []);
   const nonograms = await parseNonograms(files);
-  set(nonogramsAtom, nonograms);
-  set(loadingAtom, false);
+  const renderOptions = get(renderOptionsAtom);
+  set(_nonogramsAtom, nonograms);
+  set(_imageFilesAtom, []);
+  set(_loadingAtom, false);
 });
+
+export const renderOptionsAtom = atom((get) => get(_renderOptionsAtom));
