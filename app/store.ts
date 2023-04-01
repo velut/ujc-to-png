d3f749799bbf6657d564d2731c25fd1a5848549a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-01 02:11:03 +0200

    feat: update atom setters to create images

--- app/store.ts
+++ app/store.ts
@@ -1,4 +1,5 @@
 import { atom } from "jotai";
+import { createImageFiles } from "../lib/create-image-files";
 import { Nonogram } from "../lib/nonogram";
 import { parseNonograms } from "../lib/parse-nonogram";
 import { RenderOptions } from "../lib/render-image";
@@ -11,21 +12,22 @@ const _renderOptionsAtom = atom<RenderOptions>({
   recolor: false,
   scale: 1,
 });
-const _imageFilesAtom = atom<File[]>([]);
+const _imagesAtom = atom<File[]>([]);
 
 export const loadingAtom = atom((get) => get(_loadingAtom));
 export const nonogramsAtom = atom((get) => get(_nonogramsAtom));
 export const hasNonogramsAtom = atom((get) => get(nonogramsAtom).length > 0);
-export const imageFilesAtom = atom((get) => get(_imageFilesAtom));
+export const imagesAtom = atom((get) => get(_imagesAtom));
 
 export const processFilesAtom = atom(null, async (get, set, files: File[]) => {
   set(_loadingAtom, true);
   set(_nonogramsAtom, []);
-  set(_imageFilesAtom, []);
+  set(_imagesAtom, []);
   const nonograms = await parseNonograms(files);
   const renderOptions = get(_renderOptionsAtom);
+  const images = await createImageFiles(nonograms, renderOptions);
   set(_nonogramsAtom, nonograms);
-  set(_imageFilesAtom, []);
+  set(_imagesAtom, images);
   set(_loadingAtom, false);
 });
 
@@ -33,10 +35,11 @@ export const renderOptionsAtom = atom(
   (get) => get(_renderOptionsAtom),
   async (get, set, renderOptions: RenderOptions) => {
     set(_loadingAtom, true);
-    set(_imageFilesAtom, []);
+    set(_imagesAtom, []);
     set(_renderOptionsAtom, renderOptions);
     const nonograms = get(_nonogramsAtom);
-    set(_imageFilesAtom, []);
+    const images = await createImageFiles(nonograms, renderOptions);
+    set(_imagesAtom, images);
     set(_loadingAtom, false);
   }
 );
