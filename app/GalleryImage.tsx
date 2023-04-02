4bb5f4ae23d275adae2bb56fee67937d6f36f920
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-03 00:52:21 +0200

    feat: open new tab for gallery images on click

--- app/GalleryImage.tsx
+++ app/GalleryImage.tsx
@@ -2,20 +2,23 @@
 
 "use client";
 
+import Link from "next/link";
 import { twMerge } from "tailwind-merge";
 
 export default function GalleryImage({ url }: { url: string }) {
   return (
-    <img
-      className={twMerge(
-        "pixel-art h-32 w-32 rounded border object-contain shadow",
-        "bg-white", // Background color for transparent backgrounds.
-        "hover:shadow-lg",
-        "border-gray-200 dark:border-gray-800",
-        "hover:border-gray-400 dark:hover:border-gray-600"
-      )}
-      src={url}
-      alt=""
-    />
+    <Link href={url} target="_blank">
+      <img
+        className={twMerge(
+          "pixel-art h-32 w-32 rounded border object-contain shadow",
+          "bg-white", // Background color for transparent backgrounds.
+          "hover:shadow-lg",
+          "border-gray-200 dark:border-gray-800",
+          "hover:border-gray-400 dark:hover:border-gray-600"
+        )}
+        src={url}
+        alt=""
+      />
+    </Link>
   );
 }
