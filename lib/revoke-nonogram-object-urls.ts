03a75512903b0199a676a56571233be8f84ddc6d
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-04-06 01:31:50 +0200

    refactor: remove old website version

--- lib/revoke-nonogram-object-urls.ts
+++
@@ -1,7 +0,0 @@
-import { Nonogram } from "./parse-nonograms";
-
-export function revokeNonogramObjectUrls(nonograms: Nonogram[]) {
-  for (const { pngFileObjectUrl } of nonograms) {
-    URL.revokeObjectURL(pngFileObjectUrl);
-  }
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@ const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
