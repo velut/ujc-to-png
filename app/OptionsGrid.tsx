6b940ed4d1f5c689a6c9cde5b0bff692d57f7f4f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-06-16 23:53:45 +0200

    feat: add link to gridify.it

--- app/OptionsGrid.tsx
+++ app/OptionsGrid.tsx
@@ -7,11 +7,21 @@ import OptionsGridSize from "./OptionsGridSize";
 
 export default function OptionsGrid() {
 	return (
-		<div className="space-y-4">
-			<OptionsGridEnable />
-			<OptionsGridSize />
-			<OptionsGridColor />
-			<OptionsGridRadius />
+		<div className="space-y-8">
+			<div className="text-center text-sm border p-4 rounded-xl border-gray-600 dark:border-gray-400">
+				Check out{" "}
+				<a href="https://www.gridify.it" className="font-bold">
+					gridify.it
+				</a>{" "}
+				if you need an image to grid generator with more options.
+			</div>
+
+			<div className="space-y-4">
+				<OptionsGridEnable />
+				<OptionsGridSize />
+				<OptionsGridColor />
+				<OptionsGridRadius />
+			</div>
 		</div>
 	);
 }
