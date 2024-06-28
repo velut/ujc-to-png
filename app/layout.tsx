eb1ebfd7e89b9441bc8bbc68be68641c775656a9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-06-28 16:23:44 +0200

    refactor: use Readonly

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 	},
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
 		<html lang="en">
 			<body className="dark:bg-gray-800">
