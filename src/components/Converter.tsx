867eeb8572a8af37d97c1b36c5058f7b789b5107
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-02-12 16:59:07 +0100

    feat: add jotai provider

--- src/components/Converter.tsx
+++ src/components/Converter.tsx
@@ -1,13 +1,14 @@
+import { Provider } from "jotai";
 import ConverterDropzone from "./ConverterDropzone";
 import ConverterGallery from "./ConverterGallery";
 import ConverterOptions from "./ConverterOptions";
 
 export default function Converter() {
   return (
-    <>
+    <Provider>
       <ConverterDropzone />
       <ConverterGallery />
       <ConverterOptions />
-    </>
+    </Provider>
   );
 }
