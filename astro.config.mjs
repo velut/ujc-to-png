bcc8633413a8c381f0e78dfa51431b5dc5b92695
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-05 23:13:26 +0100

    build: generate third party licenses summary

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import alpinejs from "@astrojs/alpinejs";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      isProduction && (await import("./vite-plugin-licenses")).licenses(),
+    ],
   },
   integrations: [alpinejs({ entrypoint: "/src/scripts/alpine" })],
 });
---
+++ vite-plugin-licenses.ts
@@ -0,0 +1,91 @@
+import fs from "node:fs/promises";
+import rollupPluginLicense from "rollup-plugin-license";
+import packageJson from "./package.json" with { type: "json" };
+
+type License = {
+  name: string;
+  version: string;
+  license: string;
+  licenseText: string;
+  url: string;
+};
+
+export async function licenses() {
+  // "Falsy plugins will be ignored, which can be used to easily activate or deactivate plugins."
+  // See https://vitejs.dev/guide/api-plugin.html#plugins-config.
+  if (process.env.NODE_ENV !== "production") {
+    return false;
+  }
+
+  const manualDeps: License[] = [
+    {
+      name: "@tailwindcss/typography",
+      version: packageJson.devDependencies["@tailwindcss/typography"].replace(
+        "^",
+        "",
+      ),
+      license: "MIT",
+      licenseText: await fs.readFile(
+        "./node_modules/@tailwindcss/typography/LICENSE",
+        {
+          encoding: "utf-8",
+        },
+      ),
+      url: "https://github.com/tailwindlabs/tailwindcss-typography",
+    },
+    {
+      name: "tailwindcss",
+      version: packageJson.devDependencies["tailwindcss"].replace("^", ""),
+      license: "MIT",
+      licenseText: await fs.readFile("./node_modules/tailwindcss/LICENSE", {
+        encoding: "utf-8",
+      }),
+      url: "https://tailwindcss.com",
+    },
+  ];
+
+  // See https://github.com/vitejs/vite/discussions/7722 and
+  // https://github.com/mjeanroy/rollup-plugin-license and
+  // https://github.com/mjeanroy/rollup-plugin-license/issues/1362.
+  return rollupPluginLicense({
+    thirdParty: {
+      includePrivate: false,
+      allow: {
+        test: "MIT OR ISC OR 0BSD OR BSD-3-Clause OR Apache-2.0",
+        failOnUnlicensed: true,
+        failOnViolation: true,
+      },
+      output: {
+        // file: "public/third-party-licenses.txt",
+        file: `public/third-party-licenses.txt`,
+        template: (dependencies) => {
+          const foundLicenses = dependencies.map(
+            (dep): License => ({
+              name: dep.name ?? "",
+              version: dep.version ?? "",
+              license: dep.license ?? "",
+              licenseText: dep.licenseText ?? "",
+              url:
+                dep.homepage ||
+                (dep.name ? `https://www.npmjs.com/package/${dep.name}` : ""),
+            }),
+          );
+          return [
+            "",
+            ...[...manualDeps, ...foundLicenses]
+              .sort((a, b) => a.name.localeCompare(b.name))
+              .map((dep) =>
+                [
+                  `${dep.name} - v${dep.version} - ${dep.license}`,
+                  dep.url,
+                  "",
+                  dep.licenseText.trim(),
+                ].join("\n"),
+              ),
+            "",
+          ].join(`\n\n${"=".repeat(50)}\n\n`);
+        },
+      },
+    },
+  });
+}
