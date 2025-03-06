00cec4a93d1c1d7f44caa91254912c9069d9a320
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-03-06 19:22:31 +0100

    fix: limit concurrency when unzipping to prevent chrome errors

--- src/lib/unzip-nonograms.ts
+++ src/lib/unzip-nonograms.ts
@@ -1,17 +1,22 @@
 import { BlobReader, BlobWriter, ZipReader } from "@zip.js/zip.js";
+import pMap, { pMapSkip } from "p-map";
 
 export async function unzipNonograms(file: File): Promise<File[]> {
   const reader = new ZipReader(new BlobReader(file));
   const entries = await reader.getEntries();
-  const results = await Promise.allSettled(
-    entries
-      .filter((entry) => entry.filename.toLowerCase().endsWith(".ujc"))
-      .map(async (entry) => {
+  const ujcEntries = entries.filter(({ filename }) =>
+    filename.toLowerCase().endsWith(".ujc"),
+  );
+  return pMap(
+    ujcEntries,
+    async (entry) => {
+      try {
         const data = await entry.getData!(new BlobWriter());
         return new File([data], entry.filename.replace("MyNonograms/", ""));
-      }),
+      } catch {
+        return pMapSkip;
+      }
+    },
+    { concurrency: 8 },
   );
-  return results
-    .filter((res) => res.status === "fulfilled")
-    .map((res) => res.value);
 }
